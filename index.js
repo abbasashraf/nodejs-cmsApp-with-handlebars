df635cae1206a1884632ec92f078578414a1aaf1
abbasashraf <abbas.ashraf19@gmail.com>
2018-03-09 20:44:18 +0500

    fixed mongoURL

--- index.js
+++ index.js
@@ -87,7 +87,17 @@ app.use("/admin/posts", posts);
 app.use("/admin/categories", categories);
 app.use("/admin/comments", comments);
 
-
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ');
+});
+// If the connection throws an error
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
 
 const port = process.env.PORT || 4444 
 
