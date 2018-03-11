2a187e368e2d13ba04db43d55c9b35c202c833c6
abbasashraf <abbas.ashraf19@gmail.com>
2018-03-11 11:18:20 +0500

    fixed mongo url

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
 
-    // mongoDbUrl: 'mongodb://cms-abbas:cms-abbas@ds147274.mlab.com:47274/cms'
+     mongoDbUrl: 'mongodb://cms-abbas:cms-abbas@ds147274.mlab.com:47274/cms'
 
-    mongoDbUrl: 'mongodb://127.0.0.1:27017/cms'
+    //mongoDbUrl: 'mongodb://127.0.0.1:27017/cms'
 
 
 }
